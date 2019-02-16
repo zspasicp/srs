d9468133462b9a0048dd6d74e40f06cfb9d0bf15
Asim Sabic <asim2809sabic@hotmail.com>
2019-02-16 12:42:59 +0100

    Sql injection module initialization

---
+++ app/utils/sql_injection.py
@@ -0,0 +1,29 @@
+import requests as req
+from utils.levenshtein_distance import similarity
+
+
+injection_patterns = ["'", "'or1=1;#"]
+
+initial_vulnerable_key_words = ['sql', 'database', 'odbc']
+
+def inject_url(url: str):
+    vulnerable_key_word = []
+    resp = req.get(url)
+    #print(resp.text)
+    html1 = rest.text
+    for kw in initial_vulnerable_key_words:
+        if kw not in html1:
+            vulnerable_key_word.append(kw)
+    for ip in injection_patterns:
+        resp = req.get(url)
+        #print(resp.text)
+        html2 = rest.text
+        # for kw in vulnerable_key_word:
+            # if kw in html2:
+                # return True
+    print(similarity(html1, html2))
+    
+    
+    
+if __name__ == '__main__':
+    inject_url('https://stackoverflow.com/questions/2349991/how-to-import-other-python-files')
